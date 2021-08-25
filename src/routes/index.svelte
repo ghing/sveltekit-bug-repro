27d65a5601a2b118201477525c5ad942686cbdc9
Geoff Hing <geoff.hing@gannett.com>
2021-08-24 19:25:22 -0700

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<style>
+		/* Typography */
+
+		@font-face {
+			font-family: 'roboto';
+			src: url('./fonts/roboto-v27-latin-regular.woff2') format('woff2');
+		}
+
+    p {
+      font-family: 'roboto';
+    }
+  </style>
+</svelte:head>  
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
